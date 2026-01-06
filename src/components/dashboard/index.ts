c04f56a717206f5b0f060f7bbf70e0e25743d792
FilipiWanderley <filipi@wanderley.dev>
2026-01-06 15:36:47 -0300

    Dev: Fix index configuration

---
+++ src/components/dashboard/index.ts
@@ -0,0 +1,4 @@
+export { DashboardGestor } from './DashboardGestor';
+export { DashboardRecepcao } from './DashboardRecepcao';
+export { DashboardProfessor } from './DashboardProfessor';
+export { DashboardAluno } from './DashboardAluno';
